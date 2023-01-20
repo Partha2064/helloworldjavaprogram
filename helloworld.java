96b7f360ed75a3fc43c115ec375a5ee2af0d8ab9
Partha2064 <115972085+Partha2064@users.noreply.github.com>
2023-01-20 16:13:55 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch27 team lets get intothe business");
-      System.out.println("Hello batch27 students and training practice with Partha as cloud DevOps and setting up jenkins job ");
+      System.out.println("Hello batch27 students and training practice with Partha as cloud DevOps and setting up jenkins job and see if poll scm works");
     }  
 }  
